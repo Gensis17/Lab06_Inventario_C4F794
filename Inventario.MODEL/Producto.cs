14b0e68278fc4ebcf78052b2659c623758cce19c
Alonso Chavarria <alons182@hotmail.com>
2026-04-14 13:48:32 -0600

    Agregar archivos de proyecto.

---
+++ Inventario.MODEL/Producto.cs
@@ -0,0 +1,12 @@
+﻿namespace Inventario.MODEL
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
