7012c3839cbc505845e5b8f6a14a98431a034e4f
Genesis Gutierrez <Gggensis2005@gmail.com>
2026-05-11 01:36:30 -0600

    Base de datos

---
+++ SQLInventario.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Productos] (
+    [Id] INT IDENTITY (1,1) NOT NULL,
+    [Nombre] NVARCHAR(100) NOT NULL,
+    [Categoria] NVARCHAR(100) NOT NULL,
+    [Precio] FLOAT(53) NOT NULL,
+    [Stock] INT NOT NULL,
+    CONSTRAINT [PK_Productos] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
